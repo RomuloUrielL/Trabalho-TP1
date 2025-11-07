80a8159cb52dc285b36c1f7ccd301fb3fb52dbb8
arthurtyr <arthurcm2015@gmail.com>
2025-11-07 16:07:38 -0300

    Criação da interfaces.hpp

--- interfaces.hpp
+++ interfaces.hpp
@@ -1,26 +1,6 @@
-#ifndef INTERFACES_HPP_INCLUDED
-#define INTERFACES_HPP_INCLUDED
+#ifndef INTERFACES_H_INCLUDED
+#define INTERFACES_H_INCLUDED
 
-#include "dominios.hpp"
-#include "entidades.hpp"
 
-/*
-@brief Interface responsável por prover os serviços ao módulo inicialização para
-autenticação de usuários do sistema.
-@details É implementada pelo Módulo Apresentação-Autenticação e solicitada pelo
-Módulo Apresentação-Inicialização para validar um usuário no sistema.
-*/
-class IA_Autenticacao{
-    public:
-    virtual bool autenticar(Email&) = 0;
-};
 
-class IA_Pessoal{
-    public:
-    virtual void criar() = 0;
-    //modificar o retorno da função executar porque pode dar problema caso o
-    //usuário apague a própia conta.
-    virtual void executar(Email) = 0;
-};
-
-#endif // INTERFACES_HPP_INCLUDED
+#endif // INTERFACES_H_INCLUDED
