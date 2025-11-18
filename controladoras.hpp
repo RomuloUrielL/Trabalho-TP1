6805d4d7af52d801433715756fbef2b70c88edad
Romulo <urielliberal05@gmail.com>
2025-11-18 17:31:17 -0300

    Adicionando documentação na classe controladora da camada de apresentação relacionada ao sistema de autenticação.

--- controladoras.hpp
+++ controladoras.hpp
@@ -12,11 +12,34 @@ class IS_Autenticacao;
 class IS_Pessoal;
 class IS_Reservas;
 
+/**
+@brief Controladora da Camada de Apresentação responsável pela Autenticação
+dos usuários.
+@details Esta classe implementa a Interface Apresentação Autenticação. Tem o
+objetivo servir como intermediária entre o usuário e a camada de Serviço.
+ */
 class CntrIUAutenticacao: public IA_Autenticacao{
     private:
         IS_Autenticacao *cntrServicoAutenticacao;
     public:
+        /**
+        @brief Executa o fluxo de interação com o usuário para login.
+        @details Este método entra em um loop solicitando Email e Senha ao usuário.
+        Ele trata exceções de formato lançadades pelos métodos validar das
+        classes domínios e, após serem validados os formatos, repassa para
+        o método autenticar da camada de serviço.
+        @param email Referência para um objeto da classe dompinio Email. Se a
+        autenticação ocorrer com sucesso o objeto é preenchido com o email do
+        usuário autenticado.
+        @return true caso o email seja autenticado com sucesso e false caso o email
+        não consiga ser autenticado com sucesso.
+        */
         bool autenticar(Email&) override;
+        /**
+        @brief Responsável por criar o link com a camada de serviço. Implementa a
+        relação de dependência da arquitetura do sistema.
+        @param Ponteiro para um objeto que implemente a interface Serviço Autenticação.
+        */
         void setCntrSAutenticacao(IS_Autenticacao*);
 };
 
